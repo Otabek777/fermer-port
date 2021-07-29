2c309dc028f94108b773025b417dc77055023dd0
Otabek_Jurayev <otabek.jurayev.2002@gmail.com>
2021-07-29 13:44:58 +0500

    finish catalog

--- js/main.js
+++ js/main.js
@@ -14,3 +14,29 @@ document.querySelector('.catalog-btn').addEventListener('click', function() {
     this.classList.toggle('active');
     document.querySelector('.catalog_menu').classList.toggle('active');
 });
+
+const catalogLink = document.querySelectorAll('.catalog_menu__li p');
+const catalogClose = document.querySelector('.catalog_menu__close');
+const catalogBack = document.querySelectorAll('.catalog_menu__back');
+
+for(let i = 0; i < catalogLink.length; i++) {
+    catalogLink[i].addEventListener('click', function() {
+        this.classList.add('active');
+    });
+};
+
+for(let i = 0; i <catalogBack.length; i++) {
+    catalogBack[i].addEventListener('click', function() {
+        for(let i = 0; i < catalogLink.length; i++) {
+            catalogLink[i].classList.remove('active');
+        };
+    });
+}
+
+catalogClose.addEventListener('click', function() {
+    for(let i = 0; i < catalogLink.length; i++) {
+        catalogLink[i].classList.remove('active');
+    };
+    document.querySelector('.catalog_menu').classList.remove('active');
+    document.querySelector('.catalog-btn').classList.remove('active');
+});
